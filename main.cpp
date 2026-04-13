813687e09f84da1ab68fd69174364a91932fd1b5
EricMontez <eric.alexander.montez@gmail.com>
2026-04-13 13:18:16 -0700

    q5q1 Done

--- main.cpp
+++ main.cpp
@@ -3,19 +3,23 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int num;
+
     // TODO: ask user for input
-    cin >> n;
-    // TODO: outer loop for each row
-    for (int i = 0; i < n; i++)
+    cin >> num;
+
+    // outer loop for each row
+    for (int i = 0; i < num; i++)
     {
-        // TODO: inner loop to print letters for this row
-        for (int j = 0; j < n; j++)
+        // print letters from A to the i-th letter
+        for (int j = 0; j <= i; j++)
         {
-            // TODO: print the letter for this column
             cout << char('A' + j);
+            if (j < i)
+                cout << " ";
         }
-        // TODO: print newline after each row
+
+        // newline after each row
         cout << endl;
     }
     return 0;
