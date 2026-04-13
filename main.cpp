0c91eca7431896ae227575e9a59237409279ac6c
EricMontez <eric.alexander.montez@gmail.com>
2026-04-13 12:45:31 -0700

    q5q1 first attempt

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@ int main()
 {
     int n;
     // TODO: ask user for input
-
+    cin >> n;
     // TODO: outer loop for each row
-
+    for (int i = 0; i < n; i++)
+    {
         // TODO: inner loop to print letters for this row
-
+        for (int j = 0; j < n; j++)
+        {
             // TODO: print the letter for this column
-
+            cout << char('A' + j);
+        }
         // TODO: print newline after each row
-
-
+        cout << endl;
+    }
     return 0;
 }
